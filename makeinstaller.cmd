201a7e0bd0509f1a9e246303c3edce0d7a1ccc2d
hjschmidt99 <81195606+hjschmidt99@users.noreply.github.com>
2023-08-05 12:21:33 +0200

    fix installer

--- makeinstaller.cmd
+++ makeinstaller.cmd
@@ -1,2 +1,3 @@
-pyinstaller.exe --add-binary %USERPROFILE%\AppData\Local\Programs\Python\Python311\Lib\site-packages\pyzbar\libiconv.dll;. --add-binary %USERPROFILE%\AppData\Local\Programs\Python\Python311\Lib\site-packages\pyzbar\libzbar-64.dll;. --onefile QRscan.py
+set pyzbar=%USERPROFILE%\AppData\Local\Programs\Python\Python311\Lib\site-packages\pyzbar
+pyinstaller.exe --add-binary %pyzbar%\libiconv.dll;. --add-binary %pyzbar%\libzbar-64.dll;. --onefile QRscan.py
 pause
